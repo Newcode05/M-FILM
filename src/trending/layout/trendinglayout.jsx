e2123e25cbfe93eae5604f9af2cdef220ebdbabc
Newcode05 <11236151@st.neu.edu.vn>
2025-04-14 13:27:18 +0700

    Fix Responsive Trending

--- src/trending/layout/trendinglayout.jsx
+++ src/trending/layout/trendinglayout.jsx
@@ -7,6 +7,7 @@ function TrendingLayout() {
         { 'poster': '/public/Img-Poster/poster-disney-the-good-dinosaur.jpeg', infor: { 'name': 'Dinosaur', 'star': '8.9', 'view': '4.8M', 'time': '' } },
         { 'poster': '/public/Img-Poster/poster-8.jpg', infor: { 'name': 'Avatar', 'star': '9.0', 'view': '1.2M', 'time': '' } },
         { 'poster': '/public/Img-Poster/poster-8.jpg', infor: { 'name': 'Avatar', 'star': '9.4', 'view': '1M', 'time': '' } },
+        { 'poster': '/public/Img-Poster/poster-8.jpg', infor: { 'name': 'Avatar', 'star': '9.0', 'view': '1.2M', 'time': '' } },
 
     ]
     return (
