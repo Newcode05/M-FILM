0f87706f040d083bd05e3b7eaaf8844a015c47fa
Newcode05 <11236151@st.neu.edu.vn>
2025-04-14 14:49:59 +0700

    Fix Trending Infor

--- src/trending/component/card/trendingcard.jsx
+++ src/trending/component/card/trendingcard.jsx
@@ -3,8 +3,11 @@ import styles from "./trendingcard.module.css";
 function TrendingCard({ card }) {
     return (
         <div className={styles['trending-card']}>
-            <img className={styles['poster']} src={card.poster} alt="" />
-            <img className={styles['medal']} src="/Trending/medal-regular-36.png" alt="" />
+            <img className={styles['poster']} src={card.poster} alt="poster" />
+            {card.top <= 3 ?
+                <img className={styles['medal']}
+                    src={card.top == 1 ? "/Trending/top-1.png" : card.top == 2 ? "/Trending/top-2.png" : card.top == 3 ? "/Trending/top-3.png" : null}
+                    alt="" /> : <div className={styles['top']}>Top {card.top}</div>}
             <TrendingInfor infor={card.infor} />
         </div>
     )
